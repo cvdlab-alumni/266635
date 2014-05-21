692bfc9038ed7560b841e064587f242e59f971c3
Andrea D'Angelo <and.cino@gmail.com>
2014-05-21 20:46:25 +0200

    exercise_4

---
+++ 2014-05-16/python/exercise4.py
@@ -0,0 +1,15 @@
+# Exercise 4: Andrea D'Angelo 266635, Homework 3
+from pyplasm import *
+from scipy import *
+
+import os,sys
+
+# N.B. : PER UNA CORRETTA VISUALIZZAZIONE, SOSTITUIRE IL PATH CORRENTE 
+#        CON QUELLO DOVE E' SITUATA LA LIBRERIA lar-cc .
+sys.path.insert(0, '/Library/Python/2.7/site-packages/lar-cc/lib/py/')
+
+from sysml import *
+
+''' Exercise 4 : '''
+
+print 'I am Sorry!'
