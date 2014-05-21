498d288852fe6b4f8746e2fbe61c0bb86ef692fb
Andrea D'Angelo <and.cino@gmail.com>
2014-05-21 13:42:06 +0200

    fixed_exercise_1

--- 2014-05-16/python/exercise1.py
+++ 2014-05-16/python/exercise1.py
@@ -1,413 +1,239 @@
-# Exercise 1: Andrea D'Angelo 266635, Homework 3
+""" Andrea D'Angelo, 266635, exercise 1 """
+import os,sys
+sys.path.insert(0, '/Library/Python/2.7/site-packages/lar-cc/lib/py/')
+from sysml import *
 from pyplasm import *
 from scipy import *
 
-import os,sys
+# Visualizer:
 
-# N.B. : PER UNA CORRETTA VISUALIZZAZIONE, SOSTITUIRE IL PATH CORRENTE 
-#        CON QUELLO DOVE E' SITUATA LA LIBRERIA lar-cc .
+DRAW = COMP([VIEW,STRUCT,MKPOLS])
 
-sys.path.insert(0, '/Library/Python/2.7/site-packages/lar-cc/lib/py/')
-from lar2psm import *
-from simplexn import *
-from larcc import *
-from largrid import *
-from mapper import *
-from boolean import *
-from sysml import *
+""" Diagrams: """
 
-DRAW = COMP ([VIEW,STRUCT,MKPOLS])
+# Apartment:
 
-# Camera 1:
+apartment = assemblyDiagramInit([11,11,3])([[.3,3.5,.1,3.3,.1,2.3,.1,1.3,.3,1.8,.3],
+											[.3,1.6,.3,1.9,.1,1,.1,1,.1,4.7,.3],[.3,3,.3]])
 
-camera_1 = assemblyDiagramInit([3,3,2])([[.3,3.2,.3],[.3,4.4,.3],[.3,2.7]])
 
-V,CV = camera_1
+# Generate hpc object:
 
-hpc_camera_1 = SKEL_1(STRUCT(MKPOLS(camera_1)))
+hpc_apartment = SKEL_1(STRUCT(MKPOLS(apartment)))
 
-#VIEW (hpc_camera_1)
+# Numbering cells:
 
-#hpc_camera_1 = cellNumbering (camera_1,hpc_camera_1)(range(len(CV)),GREEN,2)
+hpc_apartment = cellNumbering (apartment,hpc_apartment)(range(len(apartment[1])),YELLOW,1)
 
-#VIEW (hpc_camera_1)
+#VIEW(hpc_apartment)
 
-toRemove = [3,9,15]
+""" Doors: """
 
-camera_1 = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
+# Entry Door:
 
-#DRAW (camera_1)
+entry_door = assemblyDiagramInit([3,1,2])([[1.2,.9,1.2],[.3],[2.1,.9]])
 
-V,CV = camera_1
+hpc_entry_door = SKEL_1(STRUCT(MKPOLS(entry_door)))
 
-hpc_camera_1 = (STRUCT(MKPOLS(camera_1)))
+hpc_entry_door = cellNumbering(entry_door,hpc_entry_door)(range(len(entry_door[1])),GREEN,1)
 
-#hpc_camera_1 = cellNumbering (camera_1,hpc_camera_1)(range(len(CV)),GREEN,2)
+#VIEW(hpc_entry_door)
 
-# Finestra
+# Kitchen Door:
 
-base = CUBOID([.3,4.4,.7])
+kitchen_door = assemblyDiagramInit([1,3,2])([[.1],[.55,.8,.55],[2.1,.9]])
 
-base = T([2,3])([0.3,0.3])(base)
+hpc_kitchen_door = SKEL_1(STRUCT(MKPOLS(kitchen_door)))
 
-base_2 = T(3)(2)(base)
+hpc_kitchen_door = cellNumbering(kitchen_door,hpc_kitchen_door)(range(len(kitchen_door[1])),GREEN,1)
 
-base_finestra = STRUCT([base,base_2])
+#VIEW(hpc_kitchen_door)
 
-lato = CUBOID([.3,1.7,1.3])
+# Room 1 door:
 
-lato = T([2,3])([0.3,1])(lato)
+room_1_door = assemblyDiagramInit([1,3,2])([[.1],[1.9,.8,2],[2.1,.9]])
 
-lato_2 = T(2)(2.8)(lato)
+hpc_room_1_door = SKEL_1(STRUCT(MKPOLS(room_1_door)))
 
-lati_finestra = STRUCT([lato,lato_2])
+hpc_room_1_door = cellNumbering(room_1_door,hpc_room_1_door)(range(len(room_1_door[1])),GREEN,1)
 
-finestra = STRUCT([base_finestra,lati_finestra])
+#VIEW(hpc_room_1_door)
 
-hpc_camera_1 = STRUCT([finestra,hpc_camera_1])
+# Room 2 door:
 
-#VIEW (hpc_camera_1)
+room_2_door = assemblyDiagramInit([1,3,2])([[.1],[1.9,.8,2],[2.1,.9]])
 
-# Salone:
+hpc_room_2_door = SKEL_1(STRUCT(MKPOLS(room_2_door)))
 
-salone = assemblyDiagramInit([3,3,2])([[.3,3.2,.3],[.3,4.4,.3],[.3,2.7]])
+hpc_room_2_door = cellNumbering(room_2_door,hpc_room_2_door)(range(len(room_2_door[1])),GREEN,1)
 
-V,CV = salone
+#VIEW(hpc_room_2_door)
 
-hpc_salone = SKEL_1(STRUCT(MKPOLS(salone)))
+# Bathe door:
 
-#VIEW (hpc_salone)
+bathe_door = assemblyDiagramInit([1,3,2])([[.1],[.1,.8,.1],[2.1,.9]])
 
-#hpc_salone = cellNumbering (salone,hpc_salone)(range(len(CV)),GREEN,2)
+hpc_bathe_door = SKEL_1(STRUCT(MKPOLS(bathe_door)))
 
-#VIEW (hpc_salone)
+hpc_bathe_door = cellNumbering(bathe_door,hpc_bathe_door)(range(len(bathe_door[1])),GREEN,1)
 
-toRemove = [3,7,9,11,15]
+#VIEW(hpc_bathe_door)
 
-salone = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
+# Closet door:
 
-#DRAW (salone)
+closet_door = assemblyDiagramInit([1,3,2])([[.1],[.1,.8,.1],[2.1,.9]])
 
-V,CV = salone
+hpc_closet_door = SKEL_1(STRUCT(MKPOLS(closet_door)))
 
-hpc_salone = (STRUCT(MKPOLS(salone)))
+hpc_closet_door = cellNumbering(closet_door,hpc_closet_door)(range(len(closet_door[1])),GREEN,1)
 
-#hpc_salone = cellNumbering (salone,hpc_salone)(range(len(CV)),GREEN,2)
+#VIEW(hpc_closet_door)
 
-# Porta
+# Shower room:
 
-lato_porta_1 = CUBOID([.3,.3,2.1])
+shower_room = assemblyDiagramInit([3,1,1])([[.4,1,.4],[.1],[3]])
 
-lato_porta_1 = T([2,3])([.3,.3])(lato_porta_1)
+hpc_shower_room = SKEL_1(STRUCT(MKPOLS(shower_room)))
 
-lato_porta_2 = CUBOID([.3,2.8,2.1])
+hpc_shower_room = cellNumbering(shower_room,hpc_shower_room)(range(len(shower_room[1])),GREEN,1)
 
-lato_porta_2 = T([2,3])([2,.3])(lato_porta_2)
+#VIEW(hpc_shower_room)
 
-sopra_porta = CUBOID([.3,4.4,.6])
+""" Windows: """
 
-sopra_porta = T([2,3])([.3,2.4])(sopra_porta)
+# Kitchen window:
 
-porta = STRUCT([lato_porta_1,lato_porta_2,sopra_porta])
+kitchen_window = assemblyDiagramInit([1,3,3])([[.3],[.1,1.4,.1],[.75,1.4,.85]])
 
-# Porta 2:
+hpc_kitchen_window = SKEL_1(STRUCT(MKPOLS(kitchen_window)))
 
-lato_porta_2_1 = CUBOID([.3,2.8,2.1])
+hpc_kitchen_window = cellNumbering(kitchen_window,hpc_kitchen_window)(range(len(kitchen_window[1])),GREEN,1)
 
-porta_2 = STRUCT([lato_porta_1,lato_porta_2_1,sopra_porta])
+#VIEW(hpc_kitchen_window)
 
-porta_2 = T([1,2])([3.5,-.1])(porta)
+# Lounge window:
 
+lounge_window = assemblyDiagramInit([3,1,2])([[1.05,1.2,1.05],[.3],[2.15,0.85]])
 
-# Finestra
+hpc_lounge_window = SKEL_1(STRUCT(MKPOLS(lounge_window)))
 
-base = CUBOID([3.5,.3,.7])
+hpc_lounge_window = cellNumbering(lounge_window,hpc_lounge_window)(range(len(lounge_window[1])),GREEN,1)
 
-base = T([1,2,3])([0.3,4.7,.3])(base)
+#VIEW(hpc_lounge_window)
 
-base_2 = T(3)(2)(base)
+# Bathe window:
 
-base_finestra = STRUCT([base,base_2])
+bathe_window = assemblyDiagramInit([3,1,3])([[.6,.6,.6],[.3],[0.75,1.4,0.85]])
 
-lato = CUBOID([1,.3,1.3])
+hpc_bathe_window = SKEL_1(STRUCT(MKPOLS(bathe_window)))
 
-lato = T([1,2,3])([.3,4.7,1])(lato)
+hpc_bathe_window = cellNumbering(bathe_window,hpc_bathe_window)(range(len(bathe_window[1])),GREEN,1)
 
-lato_2 = T(1)(2.3)(lato)
+#VIEW(hpc_bathe_window)
 
-lati_finestra = STRUCT([lato,lato_2])
+# Room 1 window:
 
-finestra = STRUCT([base_finestra,lati_finestra])
+room_1_window = assemblyDiagramInit([3,1,3])([[1.15,1.2,1.15],[.3],[.75,1.4,.85]])
 
-hpc_salone = STRUCT([finestra,porta,porta_2,hpc_salone])
+hpc_room_1_window = SKEL_1(STRUCT(MKPOLS(room_1_window)))
 
+hpc_room_1_window = cellNumbering(room_1_window,hpc_room_1_window)(range(len(room_1_window[1])),GREEN,1)
 
-#VIEW (hpc_salone)
+#VIEW(hpc_room_1_window)
 
-salone_traslato = T(1)(3.5)(hpc_salone)
+# Room 2 window:
 
-camera_salone = STRUCT([hpc_camera_1,salone_traslato])
+room_2_window = assemblyDiagramInit([3,1,3])([[.55,1.2,.55],[.3],[.75,1.4,.85]])
 
-#VIEW(camera_salone)
+hpc_room_2_window = SKEL_1(STRUCT(MKPOLS(room_2_window)))
 
-# Camera 2:
+hpc_room_2_window = cellNumbering(room_2_window,hpc_room_2_window)(range(len(room_2_window[1])),GREEN,1)
 
-camera_2 = assemblyDiagramInit([3,3,2])([[.3,2.9,.3],[.3,2.9,.3],[.3,2.7]])
+#VIEW(hpc_room_2_window)
 
-V,CV = camera_2
+""" Balcony ( I forgot it! XD ): """
 
-hpc_camera_2 = SKEL_1(STRUCT(MKPOLS(camera_2)))
+balcony = assemblyDiagramInit([3,2,3])([[.1,2.2,.1],[1,.1],[.3,1,.1]])
 
-#hpc_camera_2 = cellNumbering (camera_2,hpc_camera_2)(range(len(CV)),GREEN,2)
+hpc_balcony = SKEL_1(STRUCT(MKPOLS(balcony)))
 
-#VIEW(hpc_camera_2)
+hpc_balcony = cellNumbering(balcony,hpc_balcony)(range(len(balcony[1])),GREEN,1)
 
-toRemove = [3,7,9]
+#VIEW(hpc_balcony)
 
-camera_2 = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
+# Removing useless cells:
 
-#DRAW (camera_2)
+toRemove = [7,8]
 
-V,CV = camera_2
+V,CV = balcony
 
-hpc_camera_2 = (STRUCT(MKPOLS(camera_2)))
+balcony = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
 
-#hpc_camera_2 = cellNumbering (camera_2,hpc_camera_2)(range(len(CV)),GREEN,2)
+#DRAW (balcony)
 
-#VIEW (hpc_camera_2)
+V,CV = balcony
 
-hpc_camera_2 = T([1,2])([7,1.5])(hpc_camera_2)
+hpc_balcony = STRUCT(MKPOLS(balcony))
 
-final = STRUCT([camera_salone,hpc_camera_2])
+hpc_balcony = T([1,2])([4.35,11.4])(hpc_balcony)
 
-final = T([1,2])([5,5])(final)
+#VIEW(hpc_balcony)
 
-#VIEW (final)
+""" Assembly apartment with doors and windows: """
 
-# Closet:
+# Assembly doors:
 
-closet = assemblyDiagramInit([3,3,2])([[.3,2.9,.3],[.3,1.2,.3],[.3,2.7]])
+apartment = diagram2cell(entry_door,apartment,100)
+apartment = diagram2cell(kitchen_door,apartment,141)
+apartment = diagram2cell(room_1_door,apartment,94)
+apartment = diagram2cell(room_2_door,apartment,157)
+apartment = diagram2cell(closet_door,apartment,216)
+apartment = diagram2cell(bathe_door,apartment,210)
+apartment = diagram2cell(shower_room,apartment,310)
 
-V,CV = closet
+# Assembly windows:
 
-hpc_closet = SKEL_1(STRUCT(MKPOLS(closet)))
+apartment = diagram2cell(kitchen_window,apartment,262)
+apartment = diagram2cell(lounge_window,apartment,128)
+apartment = diagram2cell(bathe_window,apartment,296)
+apartment = diagram2cell(room_1_window,apartment,64)
+apartment = diagram2cell(room_2_window,apartment,190)
 
-#hpc_closet = cellNumbering (closet,hpc_closet)(range(len(CV)),GREEN,2)
+# Renumbering apartment cells:
 
-#VIEW (hpc_closet)
+hpc_apartment = SKEL_1(STRUCT(MKPOLS(apartment)))
+hpc_apartment = cellNumbering (apartment,hpc_apartment)(range(len(apartment[1])),YELLOW,1)
+#VIEW(hpc_apartment)
 
-toRemove = [3,7,9,11]
+# Removing useless cells, like: roof, doors, windows etc.
 
-closet = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
+emptyChain = [2,5,8,11,14,17,20,23,26,29,32,35,38,41,44,47,50,53,56,59,62,64,67,70,73,76,79,82,
+			85,88,91,93,96,98,101,104,107,110,113,116,119,122,125,127,130,133,136,138,141,144,147,
+			150,153,155,158,161,164,167,170,173,176,179,182,185,188,190,193,196,199,202,205,207,210,
+			212,215,218,221,224,227,230,233,236,239,242,245,248,251,254,257,259,262,265,268,271,274,
+			277,280,283,286,289,292,294,297,300,303,305,308,311,314,317,320,323,326,329,332,335,338,
+			341,344,347,350,0,3,6,9,12,15,18,21,1,4,7,10,13,16,19,22,33,36,39,42,45,48,51,54,34,37,
+			40,43,46,49,52,55,312,315,345,348,313,316,346,349,287,290,318,321,288,291,319,322,100,
+			103,106,109,112,115,118,121,124,61,187,217,250,163,166,169,195,198,201,226,229,232,296,
+			299,302,307,270,238,244,175,178,181,143,146,149,353,359,365,371,377,383,388,394,409,418,
+			401,427]
 
-DRAW (closet)
+solidCV = [cell for k,cell in enumerate(apartment[1]) if not (k in emptyChain)]
+exteriorCV =  [cell for k,cell in enumerate(apartment[1]) if k in emptyChain]
+exteriorCV += exteriorCells(apartment)
 
-V,CV = closet
+#DRAW((apartment[0],solidCV))
 
-hpc_closet = STRUCT(MKPOLS(closet))
+#VIEW(SKEL_1(STRUCT(MKPOLS([apartment[0],solidCV]))))
 
-#hpc_closet = cellNumbering (closet,hpc_closet)(range(len(CV)),GREEN,2)
+''' Final Apartment: '''
 
-#VIEW (hpc_closet)
+final_apartment = STRUCT(MKPOLS([apartment[0],solidCV]))
 
-hpc_closet = T(1)(7)(hpc_closet)
+#VIEW(final_apartment)
 
-final = STRUCT([camera_salone,hpc_camera_2,hpc_closet])
+final_apartment = STRUCT([final_apartment,hpc_balcony])
 
-final = T([1,2])([10,10])(final)
+final_apartment = T([1,2])([10,10])(final_apartment)
 
-#VIEW (final)
-
-# ingresso:
-
-ingresso = assemblyDiagramInit([3,3,2])([[.3,1.6,.3],[.3,3.4,.3],[.3,2.7]])
-
-V,CV = ingresso
-
-hpc_ingresso = SKEL_1(STRUCT(MKPOLS(ingresso)))
-
-#VIEW (hpc_ingresso)
-
-#hpc_ingresso = cellNumbering (ingresso,hpc_ingresso)(range(len(CV)),GREEN,2)
-
-#VIEW (hpc_ingresso)
-
-toRemove = [7,9,11,15]
-
-ingresso = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
-
-#VIEW(hpc_ingresso)
-
-DRAW (ingresso)
-
-V,CV = ingresso
-
-hpc_ingresso = STRUCT(MKPOLS(ingresso))
-
-#hpc_ingresso = cellNumbering (ingresso,hpc_ingresso)(range(len(CV)),GREEN,2)
-
-#VIEW (hpc_camera_2)
-
-hpc_ingresso = T([1,2])([3.5,-5.7])(hpc_ingresso)
-
-final = STRUCT([camera_salone,hpc_camera_2,hpc_closet,hpc_ingresso])
-
-final = T([1,2])([10,10])(final)
-
-#VIEW (final)
-
-# corridoio 1:
-
-corridoio = assemblyDiagramInit([3,3,2])([[.3,3.2,.3],[.3,1.7,.3],[.3,2.7]])
-
-V,CV = corridoio
-
-hpc_corridoio = SKEL_1(STRUCT(MKPOLS(corridoio)))
-
-#VIEW (hpc_corridoio)
-
-#hpc_corridoio = cellNumbering (corridoio,hpc_corridoio)(range(len(CV)),GREEN,2)
-
-#VIEW (hpc_corridoio)
-
-toRemove = [7,9,11,15]
-
-corridoio = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
-
-DRAW (corridoio)
-
-V,CV = corridoio
-
-hpc_corridoio = STRUCT(MKPOLS(corridoio))
-
-#hpc_corridoio = cellNumbering (corridoio,hpc_corridoio)(range(len(CV)),GREEN,2)
-
-#VIEW (hpc_camera_2)
-
-hpc_corridoio = T([1,2])([3.5,-2])(hpc_corridoio)
-
-final = STRUCT([camera_salone,hpc_camera_2,hpc_closet,hpc_ingresso,hpc_corridoio])
-
-final = T([1,2])([10,10])(final)
-
-#VIEW (final)
-
-# corridoio 2:
-
-corridoio_2 = assemblyDiagramInit([3,3,2])([[.3,2.9,.3],[.3,1.7,.3],[.3,2.7]])
-
-V,CV = corridoio_2
-
-hpc_corridoio_2 = SKEL_1(STRUCT(MKPOLS(corridoio_2)))
-
-#VIEW (hpc_corridoio_2)
-
-#hpc_corridoio_2 = cellNumbering (corridoio_2,hpc_corridoio_2)(range(len(CV)),GREEN,2)
-
-#VIEW (hpc_corridoio_2)
-
-toRemove = [3,7,9]
-
-corridoio_2 = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
-
-DRAW (corridoio_2)
-
-V,CV = corridoio_2
-
-hpc_corridoio_2 = STRUCT(MKPOLS(corridoio_2))
-
-#hpc_corridoio_2 = cellNumbering (corridoio_2,hpc_corridoio_2)(range(len(CV)),GREEN,2)
-
-#VIEW (hpc_camera_2)
-
-hpc_corridoio_2 = T([1,2])([7,-2])(hpc_corridoio_2)
-
-final = STRUCT([camera_salone,hpc_camera_2,hpc_closet,hpc_ingresso,hpc_corridoio,hpc_corridoio_2])
-
-final = T([1,2])([10,10])(final)
-
-#VIEW (final)
-
-# Cucina:
-
-cucina = assemblyDiagramInit([3,3,2])([[.3,2.6,.3],[.3,3.4,.3],[.3,2.7]])
-
-V,CV = cucina
-
-hpc_cucina = SKEL_1(STRUCT(MKPOLS(cucina)))
-
-#VIEW (hpc_cucina)
-
-#hpc_cucina = cellNumbering (cucina,hpc_cucina)(range(len(CV)),GREEN,2)
-
-#VIEW (hpc_cucina)
-
-toRemove = [3,9]
-
-cucina = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
-
-DRAW (cucina)
-
-V,CV = cucina
-
-hpc_cucina = STRUCT(MKPOLS(cucina))
-
-#hpc_cucina = cellNumbering (cucina,hpc_cucina)(range(len(CV)),GREEN,2)
-
-#VIEW (hpc_cucina)
-
-# Porta cucina:
-
-porta_cucina = CUBOID([.3,2.7,2.8])
-
-porta_cucina = T([1,2,3])([0,1,.2])(porta_cucina)
-
-#VIEW(porta_cucina)
-
-hpc_cucina = STRUCT([porta_cucina,hpc_cucina])
-
-hpc_cucina = T([1,2])([5.4,-5.7])(hpc_cucina)
-
-
-
-final = STRUCT([camera_salone,hpc_camera_2,hpc_closet,hpc_ingresso,hpc_corridoio,hpc_corridoio_2,hpc_cucina])
-
-final = T([1,2])([10,10])(final)
-
-#VIEW (final)
-
-# bagno:
-
-bagno = assemblyDiagramInit([3,3,2])([[.3,1.6,.3],[.3,3.4,.3],[.3,2.7]])
-
-V,CV = bagno
-
-hpc_bagno = SKEL_1(STRUCT(MKPOLS(bagno)))
-
-#VIEW (hpc_bagno)
-
-#hpc_bagno = cellNumbering (bagno,hpc_bagno)(range(len(CV)),GREEN,2)
-
-#VIEW (hpc_bagno)
-
-toRemove = [9,11]
-
-bagno = V,[cell for k,cell in enumerate(CV) if not (k in toRemove)]
-
-DRAW (bagno)
-
-V,CV = bagno
-
-hpc_bagno = STRUCT(MKPOLS(bagno))
-
-#hpc_bagno = cellNumbering (bagno,hpc_bagno)(range(len(CV)),GREEN,2)
-
-#VIEW (hpc_camera_2)
-
-hpc_bagno = T([1,2])([8.3,-5.7])(hpc_bagno)
-
-final = STRUCT([camera_salone,hpc_camera_2,hpc_closet,hpc_ingresso,hpc_corridoio,hpc_corridoio_2,hpc_cucina,hpc_bagno])
-
-final = T([1,2])([10,10])(final)
-
-VIEW (final)
+VIEW(final_apartment)
